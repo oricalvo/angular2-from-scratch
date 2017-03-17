c306adef19b3459973e50c510d3c134667fbc5e3
ocalvo <ori.calvo@gmail.com>
2017-03-17 10:44:21 +0200

    Fixing app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "my-app",
-    template: "<h1>Hello Angular2 123</h1>"
+    template: "<h1>Hello Angular2 XXX</h1>"
 })
 export class AppComponent {
 }
